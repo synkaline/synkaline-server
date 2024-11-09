292819bcad9c4590c9330dcf2a48db8ebe4bbab3
itsAnanth <danonymousarmy@gmai.com>
2024-11-09 20:16:24 +0530

    feat(`*`): add server side audio queue tracking

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { HttpServer } from '@/server/HttpServer';
 import { Logger } from '@/utils/Logger'
+import ytdl from '@distube/ytdl-core';
+import { Queue, QueueItem } from '@/structures/Queue';
 
 Logger.DEV = true;
 
@@ -21,4 +23,31 @@ declare module 'websocket' {
     httpServer.start()
 
     console.log(httpServer.events)
+
+    let queue = new Queue()
+
+    let links = [
+        "https://www.youtube.com/watch?v=J88v6SomV0k",
+        "https://www.youtube.com/watch?v=6yrdS4tIP9U",
+
+    ]
+
+    for (let i = 0; i < links.length; i++) {
+        let info = await ytdl.getInfo(links[i])
+        let format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' })
+        queue.addItem({
+            info,
+            format,
+            startTime: Date.now()
+        })
+    }
+
+    
+
+    
+    queue.on('newtrack', (t: QueueItem) => console.log(t.info.videoDetails.title))
+
+    queue.start();
+
+    // console.log(ytdl.chooseFormat(info.formats, { quality: 'highestaudio' }))
 })()
---
+++ src/structures/Queue/Queue.ts
@@ -0,0 +1,50 @@
+import Logger from "@/utils/Logger";
+import type { videoInfo, videoFormat } from "@distube/ytdl-core"
+import EventEmitter from "events";
+
+type QueueItem = {
+    info: videoInfo,
+    format: videoFormat
+    startTime: ReturnType<typeof Date['now']> | null
+}
+class Queue extends EventEmitter{
+
+    current: number;
+    items: QueueItem[];
+
+
+    constructor() {
+        super();
+        this.current = -1;
+        this.items = [];
+
+    }
+
+    start() {
+        this.tick();
+    }
+
+    tick() {
+        this.current++;
+
+        if (this.current > this.items.length - 1) {
+            Logger.log('[QUEUE]', 'queue exhausted, restart queue')
+            // this.current = 0
+            return
+        }
+
+        this.emit('newtrack', (this.items[this.current]))
+
+        let duration = parseInt(this.items[this.current].info.videoDetails.lengthSeconds) * 1000
+        setTimeout(this.tick.bind(this), duration);
+    }
+
+    addItem(item: QueueItem) {
+        this.items.push(item)
+    }
+
+}
+
+export default Queue;
+export type { QueueItem };
+export { Queue };
